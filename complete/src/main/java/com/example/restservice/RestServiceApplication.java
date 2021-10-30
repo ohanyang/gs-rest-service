fa55a4c999b83c2066cbbd1a244d2ccab0281855
ohanyang <61538053+ohanyang@users.noreply.github.com>
2021-10-30 11:08:27 +0800

    Update RestServiceApplication.java

--- complete/src/main/java/com/example/restservice/RestServiceApplication.java
+++ complete/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestServiceApplication {
 
     //Some comments here test test test
+    //wut wut wut
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
     }
